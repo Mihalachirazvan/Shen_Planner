4db68ce0590d12183166d778edf3af0979c56b55
Razvi Mihalachi <razvan3105@yahoo.com>
2021-12-11 12:29:47 +0200

    vitals layouts

---
+++ app/src/main/java/com/upt/cti/shen/MainActivity.java
@@ -0,0 +1,15 @@
+package com.upt.cti.shen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
